85454db27285d8da9cfb49a03e49c7524b966a8d
Ankur02Sarkar <sarkarrohonankur@gmail.com>
2025-06-11 20:39:58 +0530

    Update hero-section.tsx

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -222,39 +222,39 @@ export default function HeroSection() {
             </div>
             <div className="mx-auto mt-12 grid max-w-2xl grid-cols-4 gap-x-12 gap-y-8 transition-all duration-500 group-hover:opacity-50 group-hover:blur-xs sm:gap-x-16 sm:gap-y-14">
               <div className="flex">
-                <img
-                  className="mx-auto h-5 w-fit"
+                <Image
+                  className="mx-auto w-fit"
                   src={SBILogo}
                   alt="SBI Logo"
-                  height="20"
-                  width="auto"
+                  height="40"
+                  width="40"
                 />
               </div>
               <div className="flex">
-                <img
-                  className="mx-auto h-4 w-fit"
+                <Image
+                  className="mx-auto w-fit"
                   src={BOBLogo}
                   alt="BOB Logo"
-                  height="16"
-                  width="auto"
+                  height="40"
+                  width="40"
                 />
               </div>
               <div className="flex">
-                <img
-                  className="mx-auto h-4 w-fit"
+                <Image
+                  className="mx-auto w-fit"
                   src={IDFCLogo}
                   alt="IDFC Logo"
-                  height="16"
-                  width="auto"
+                  height="40"
+                  width="40"
                 />
               </div>
               <div className="flex">
-                <img
-                  className="mx-auto h-5 w-fit"
+                <Image
+                  className="mx-auto w-fit"
                   src={AXISLogo}
                   alt="AXIS Logo"
-                  height="20"
-                  width="auto"
+                  height="40"
+                  width="40"
                 />
               </div>
             </div>
